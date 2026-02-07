640c736a005cbe4b32281220df570b2888ca24ab
Amonrat <109088709+guzzle999@users.noreply.github.com>
2026-02-08 01:13:49 +0700

    update my profile picture

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -15,7 +15,7 @@ const Aboutme = () => {
       >
         About<AuroraText className="ml-2">Me</AuroraText>
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-8 md:max-w-5xl max-w-6xl w-full items-center mx-auto md:px-6 p-4 border border-white/20 bg-white/10 backdrop-blur-xl shadow-xl shadow-purple-500/20 rounded-xl transition z-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-8 md:max-w-5xl max-w-6xl w-full items-center mx-auto md:px-12 p-4 border border-white/20 bg-white/10 backdrop-blur-xl shadow-xl shadow-purple-500/20 rounded-xl transition z-10">
         <div className="text-white space-y-4 text-center md:text-left">
           <p className="text-white md:text-lg leading-relaxed">
             I’m a Junior Full Stack Developer passionate about modern web
@@ -37,8 +37,7 @@ const Aboutme = () => {
             className="mb-6 relative flex px-2 py-2 md:px-6 md:py-3 rounded-xl font-semibold cursor-pointer
   text-blue-100 border border-purple-800
   backdrop-blur-xl transition-all duration-300
-  hover:text-white hover:bg-purple-500/20
-  "
+  hover:text-white hover:bg-purple-500/20"
           >
             <ArrowLeft className="mr-2" /> Back to Home
           </button>
@@ -46,7 +45,7 @@ const Aboutme = () => {
 
         <div className="md:flex justify-center md:justify-end hidden">
           <img
-            src="mypicture.jpg"
+            src="profilepic.jpg"
             alt="Profile"
             className="w-full max-w-[330px] aspect-[3/4] object-cover rounded-2xl md:my-6 transition duration-500 hover:scale-105
     hover:shadow-[0_0_45px_rgba(0,160,255,0.5)]"
